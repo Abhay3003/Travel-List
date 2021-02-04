4ab3449447894d4aeaf36dbd00480f93ff4a4517
Abhay3003 <53911492+Abhay3003@users.noreply.github.com>
2021-02-04 11:15:20 +0530

    Update location.js

--- backend/util/location.js
+++ backend/util/location.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 const HttpError = require('../models/http-error');
-const API_KEY = 'AIzaSyAfScbt_TgMCMOr6DIUm5bWWisjl48_oX4';
+const API_KEY = '';
 
 async function getCoordsForAddress (address) {
     const response = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${API_KEY}`);
